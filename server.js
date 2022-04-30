8e0ba6a861168f25fbdf474f57bff903a3904797
dev-weirdo <dev_weirdo@protonmail.com>
2022-04-30 15:00:52 +0600

    Added delete functionality

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ const run = async () => {
             res.send(item);
         })
 
+        app.delete('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await groceryCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
     }
     finally { }
